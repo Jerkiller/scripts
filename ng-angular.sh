413380616384cb9a081232ad7d1d29cd93e77fb7
Jerkiller <luca.favorido@gmail.com>
2019-02-12 10:41:47 +0100

    Create ng-angular.sh

---
+++ ng-angular.sh
@@ -0,0 +1,20 @@
+# Version of Angular CLI
+ng -v
+
+# Create angular project
+ng new <project-name>
+
+# Serve the project on host/port
+ng serve --host 127.0.0.1 --port 4200
+
+# Transpile project using the production profile
+ng build –-prod
+
+#	Generate Component
+ng g c <path/component-name>
+
+#	Generate Class
+ng g class <path/class-name>
+
+#	Generate Service
+ng g s <path/service-name>
